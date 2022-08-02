48b2916a1860f399c3c7e13bd6047fc2373331c8
felipe <felipesscreator@gmail.com>
2022-08-02 15:05:02 -0300

    docker failed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { MenuModule } from './menu/menu.module';
 import { MesaModule } from './mesa/mesa.module';
@@ -6,8 +6,24 @@ import { AuthModule } from './auth/auth.module';
 import { OptionModule } from './option/option.module';
 // import { OptionModule } from './option/option.module';
 
+// import * as redisStore from 'cache-manager-redis-store';
+// import { ConfigModule } from '@nestjs/config';
+
 @Module({
-  imports: [UserModule, MenuModule, MesaModule, OptionModule, AuthModule],
+  imports: [
+    UserModule,
+    MenuModule,
+    MesaModule,
+    OptionModule,
+    AuthModule,
+    // ConfigModule.forRoot(),
+    // CacheModule.register({
+    //   isGlobal: true,
+    //   store: redisStore,
+    //   host: process.env.REDIS_HOST,
+    //   port: process.env.REDIS_PORT,
+    // }),
+  ],
   controllers: [],
   providers: [],
 })
